59e5371c9716a5e2d2027fd9c8961c5c2e78e5a2
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-08-12 11:23:58 -0300

    feat: refactor cards

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -6,26 +6,34 @@ interface ActivityData {
   name: string;
   description: string;
   date: string;
+  onEdit?: () => void;
+  onDelete?: () => void;
 }
 
-function ActivityCard(props: ActivityData) {
+function ActivityCard({
+  name,
+  description,
+  date,
+  onEdit,
+  onDelete,
+}: ActivityData) {
   return (
     <Card className="border-solid border-gray-300 max-w-[370px]">
       <CardContent>
         <div className="flex place-content-between">
-          <h1 className="font-semibold text-xl">{props.name}</h1>
+          <h1 className="font-semibold text-xl">{name}</h1>
           <div className="gap-1">
-            <Button variant={"ghost"}>
+            <Button variant={"ghost"} onClick={onEdit}>
               <Edit2Icon />
             </Button>
-            <Button variant={"ghost"}>
+            <Button variant={"ghost"} onClick={onDelete}>
               <TrashIcon className="text-red-600" />
             </Button>
           </div>
         </div>
-        <p className="text-muted-foreground">{props.description}</p>
+        <p className="text-muted-foreground">{description}</p>
         <p className="text-muted-foreground">
-          {new Date(props.date).toLocaleDateString()}
+          {new Date(date).toLocaleDateString()}
         </p>
       </CardContent>
     </Card>
--- src/components/TravelCard.tsx
+++ src/components/TravelCard.tsx
@@ -10,24 +10,22 @@ interface TravelData {
   id: string;
 }
 
-const TravelCard = (props: TravelData) => {
+const TravelCard = ({ destination, startDate, endDate, id }: TravelData) => {
   return (
     <Card className="border-solid border-gray-300 max-w-[370px] w-full">
       <CardContent className="flex items-center justify-between gap-2 px-3 py-4">
         <CalendarDays className="flex-shrink-0" />
 
         <div className="flex flex-col overflow-hidden min-w-0">
-          <h2 className="text-lg font-semibold truncate">
-            {props.destination}
-          </h2>
+          <h2 className="text-lg font-semibold truncate">{destination}</h2>
           <p className="text-sm text-muted-foreground truncate">
-            {new Date(props.startDate).toLocaleDateString()} -{" "}
-            {new Date(props.endDate).toLocaleDateString()}
+            {new Date(startDate).toLocaleDateString()} -{" "}
+            {new Date(endDate).toLocaleDateString()}
           </p>
         </div>
 
         <Button className="capitalize p-2 rounded-xl whitespace-nowrap" asChild>
-          <Link href={`/travels/${props.id}`}>Ver detalhes</Link>
+          <Link href={`/travels/${id}`}>Ver detalhes</Link>
         </Button>
       </CardContent>
     </Card>
